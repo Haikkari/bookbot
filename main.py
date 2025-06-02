89d79bb5c8d76749e5c79cd8c9ba72647349a9d9
Haikkari <haikkari@gmail.com>
2025-06-02 21:11:45 +0300

    added function get_book_text

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+def get_book_text(filepath):
+    """
+    Reads the content of a book from a file and returns it as a string.
+    
+    :param filepath: Path to the book file.
+    :return: Content of the book as a string.
+    """
+    with open(filepath) as file:
+        return file.read()
+    
+def main():
+    # bookpath is a relative path to book and program prints the book on terminal
+    bookpath = "books/frankenstein.txt"
+    print(get_book_text(bookpath))
+
+main()
