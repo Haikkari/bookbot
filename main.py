c5ca50dae368ee754ad92e7b4f92d897a2d0a10c
Haikkari <haikkari@gmail.com>
2025-06-03 22:02:04 +0300

    Updated main to use system arguments for bookpaths

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from stats import count_words, count_characters, sorted_character_list
+import sys
 
 def main():
-    bookpath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    bookpath = sys.argv[1]
     sorted_list = sorted_character_list(count_characters(bookpath))
 
     print("============ BOOKBOT ============")
